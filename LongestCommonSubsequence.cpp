62531fd306f7170b4752ac310e2c7f9609fd23c2
rohankundu <116959771+rohankundu@users.noreply.github.com>
2022-10-30 19:45:25 +0530

    Create LongestCommonSubsequence.cpp

---
+++ LongestCommonSubsequence.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Returns length of LCS for X, Y */
+int lcs(string X, string Y, int m, int n )
+{
+    if (m == 0 || n == 0)
+        return 0;
+    if (X[m-1] == Y[n-1])
+        return 1 + lcs(X, Y, m-1, n-1);
+    else
+        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+}
+
+/* Driver program to test */
+int main()
+{
+    string X = "OCTOBER";
+    string Y = "HACKTOBER";
+    int m = X.size();
+    int n = Y.size();
+    cout<<"Length of LCS is "<<lcs( X, Y, m, n )<<endl;
+    return 0;
+}
